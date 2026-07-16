0e4f0cd681d0afcd838096015de27e58f5bc3ec7
radfallenn <113562142+radfallenn@users.noreply.github.com>
2026-07-16 09:13:44 -0300

    Atualizar instalador Bridge para PS5 192.168.1.81

--- scripts/install-bridge-from-v408-apk.js
+++ scripts/install-bridge-from-v408-apk.js
@@ -18,20 +18,19 @@ for(const marker of markers){
 
 html=html.replace(/\s*<script\s+src=["']bridge-v408\.js["']><\/script>\s*/ig,'\n');
 html=html.replaceAll('http://192.168.1.70:8787','http://192.168.1.70:8788');
-html=html.replaceAll('192.168.1.68','192.168.1.71');
+html=html.replaceAll('192.168.1.68','192.168.1.81');
+html=html.replaceAll('192.168.1.71','192.168.1.81');
 
-// Remove somente a inicialização automática legada; as funções visuais permanecem disponíveis.
 html=html.replace(
  /\$\('bridgeUrl'\)\.value=localStorage\.getItem\('gt7_bridge_url'\)[\s\S]*?timer=setInterval\(poll,700\);/,
- "$ ('bridgeUrl')".replace(' ','')+".value='http://192.168.1.70:8788';$('ps5Ip').value='192.168.1.71';renderSegments(0);renderFuel(0);"
+ "$ ('bridgeUrl')".replace(' ','')+".value='http://192.168.1.70:8788';$('ps5Ip').value=localStorage.getItem('gt7_ps5_ip')||'192.168.1.81';renderSegments(0);renderFuel(0);"
 );
 html=html.replace(/poll\(\);\s*timer=setInterval\(poll,700\);/g,'');
 html=html.replace(/timer=setInterval\(poll,700\);/g,'');
-
 html=html.replace('</body>','<script src="bridge-v408.js"></script>\n</body>');
 
 if(!html.includes('bridge-v408.js'))throw new Error('Conexão limpa não foi injetada');
-if(html.includes('http://192.168.1.70:8787')||html.includes('192.168.1.68'))throw new Error('Configuração antiga ainda presente');
+if(html.includes('http://192.168.1.70:8787')||html.includes('192.168.1.68')||html.includes('192.168.1.71'))throw new Error('Configuração antiga ainda presente');
 if((html.match(/bridge-v408\.js/g)||[]).length!==1)throw new Error('Referência duplicada da Bridge');
 fs.writeFileSync(indexPath,html);
-console.log('Conexão limpa e única instalada: 192.168.1.70:8788 / PS5 192.168.1.71');
+console.log('Conexão GT7 instalada: Bridge 192.168.1.70:8788 / PS5 padrão 192.168.1.81');
