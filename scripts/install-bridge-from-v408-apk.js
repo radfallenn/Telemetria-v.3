5a3687eb374b27d15c38facd37cdfc7a0edc121a
radfallenn <113562142+radfallenn@users.noreply.github.com>
2026-07-28 10:53:26 -0300

    Instala controlador usando a Bridge 8789

--- scripts/install-bridge-from-v408-apk.js
+++ scripts/install-bridge-from-v408-apk.js
@@ -18,13 +18,14 @@ for(const marker of markers){
 
 const bridgeScriptTag=/<script\b(?=[^>]*\bsrc\s*=\s*["'][^"']*bridge-v408\.js(?:[?#][^"']*)?["'])[^>]*>\s*<\/script>\s*/gi;
 html=html.replace(bridgeScriptTag,'\n');
-html=html.replaceAll('http://192.168.1.70:8787','http://192.168.1.70:8788');
+html=html.replaceAll('http://192.168.1.70:8787','http://192.168.1.70:8789');
+html=html.replaceAll('http://192.168.1.70:8788','http://192.168.1.70:8789');
 html=html.replaceAll('192.168.1.68','192.168.1.81');
 html=html.replaceAll('192.168.1.71','192.168.1.81');
 
 html=html.replace(
  /\$\('bridgeUrl'\)\.value=localStorage\.getItem\('gt7_bridge_url'\)[\s\S]*?timer=setInterval\(poll,700\);/,
- "$ ('bridgeUrl')".replace(' ','')+".value='http://192.168.1.70:8788';$('ps5Ip').value=localStorage.getItem('gt7_ps5_ip')||'192.168.1.81';renderSegments(0);renderFuel(0);"
+ "$ ('bridgeUrl')".replace(' ','')+".value='http://192.168.1.70:8789';$('ps5Ip').value=localStorage.getItem('gt7_ps5_ip')||'192.168.1.81';renderSegments(0);renderFuel(0);"
 );
 html=html.replace(/poll\(\);\s*timer=setInterval\(poll,700\);/g,'');
 html=html.replace(/timer=setInterval\(poll,700\);/g,'');
@@ -32,6 +33,6 @@ html=html.replace('</body>','<script src="bridge-v408.js"></script>\n</body>');
 
 const installedBridgeTags=html.match(bridgeScriptTag)||[];
 if(installedBridgeTags.length!==1)throw new Error('Referência duplicada da Bridge: '+installedBridgeTags.length+' tags');
-if(html.includes('http://192.168.1.70:8787')||html.includes('192.168.1.68')||html.includes('192.168.1.71'))throw new Error('Configuração antiga ainda presente');
+if(html.includes('http://192.168.1.70:8787')||html.includes('http://192.168.1.70:8788')||html.includes('192.168.1.68')||html.includes('192.168.1.71'))throw new Error('Configuração antiga ainda presente');
 fs.writeFileSync(indexPath,html);
-console.log('Conexão GT7 instalada: Bridge 192.168.1.70:8788 / PS5 padrão 192.168.1.81');
+console.log('Conexão GT7 instalada: nova Bridge 192.168.1.70:8789 / PS5 padrão 192.168.1.81');
